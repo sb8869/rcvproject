a014eb4f36571d7555fd452474acdec6099724fe
Suraj Bonthu <44912512+sb8869@users.noreply.github.com>
2021-10-07 00:30:06 -0400

    RCV Files v3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 var p = document.getElementsByTagName('p');
 var choice = document.getElementsByClassName('choice');
 var dragItem = null;
-var clone = null;
 
 for (var i of p){
     i.addEventListener('dragstart', dragStart);
@@ -9,55 +8,13 @@ for (var i of p){
 }
 
 function dragStart(){
-    // console.log(this);
-    
-    // if "this" is on the right then do this
-    // how to check if this is on right or left
-    
-    // dragItem = this;
-    // clone = dragItem.cloneNode(true);
-    // console.log(this.id);
-    // this.style.backgroundColor = "darkGray";
-    // setTimeout(() => this.style.display = "block", 0);
-    // if (this.style.backgroundColor === "darkGray") {
-    //     console.log('asdf');
-    // }
-
-
-
-    if (this.id === "right") {
-        dragItem = this;
-        clone = dragItem.cloneNode(true);
-        clone.setAttribute("id", "left");
-        console.log(clone.id);
-        this.style.backgroundColor = "darkGray";
-        setTimeout(() => this.style.display = "block", 0);
-    }
-
-    else if (this.id === "left"){
-        
-        dragItem = this;
-        clone = dragItem.cloneNode(true);
-        clone.setAttribute("id", "right")
-        console.log(clone.id);
-        this.style.backgroundColor = "gray";
-        setTimeout(() => this.style.display = "none", 0);
-    }
-
-    // if "this" is on the left then do this
+    dragItem = this;
+    setTimeout(() => this.style.display = "none", 0);
 }
 
 function dragEnd(){
-    // this.append(dragItem);
-    if(clone.id === "right") {
-        clone.style.display = 'none';
-    }
     setTimeout(() => this.style.display = "block", 0);
-
-    //clone = null;
-    //dragItem = null;
-    
-    // console.log(clone);
+    dragItem = null;
 }
 
 for (j of choice){
@@ -67,25 +24,8 @@ for (j of choice){
     j.addEventListener('drop', Drop);
 }
 
-function Drop() {
-    console.log(dragItem);
-    var left = document.getElementById('left');
-    if (left !== null) {
-        left.remove();
-    }
-
-    // for (var i of p){
-    //     if (i.id === "left") {
-    //         i.remove();
-    //     }
-    // }
-    //else{
-    this.append(clone);
-    //}
-
-    // this.append(clone);
-    // clone = null;
-    // check();
+function Drop(){
+    this.append(dragItem);
 }
 
 function dragOver(e){
@@ -94,17 +34,8 @@ function dragOver(e){
 
 function dragEnter(e){
     e.preventDefault();
-    
 }
 
 function dragLeave(e){
-    e.preventDefault();
-}
 
-function check() {
-    for (var i of p){
-        if (i.id === "left") {
-            i.remove();
-        }
-    }
 }
