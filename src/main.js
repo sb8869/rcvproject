15f246702e2b189f9e91d2897216d8e756bcf93c
Suraj Bonthu <44912512+sb8869@users.noreply.github.com>
2021-10-14 00:29:46 -0400

    RCV Project v5

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
+import "./jquery-3.6.0.min.js";
+
 let p = document.getElementsByTagName('p');
 let choice = document.getElementsByClassName('choice');
 let dragItem = null;
 let dropZone = document.getElementById('dropZone');
 let leftSide = document.getElementById('leftSide');
-console.log(leftSide);
+let middleSide = document.getElementById('middleSide')
 let rightSide = document.getElementById('rightSide');
 
+let first = document.getElementById('num1');
+let second = document.getElementById('num2');
+let third = document.getElementById('num3');
+let fourth = document.getElementById('num4');
+let fifth = document.getElementById('num5');
+let submitButton = document.getElementById('numSubmit');
+let candidates = document.getElementById('candidates-change');
+
+
+
 let rankedList = [];
+
+if (rankedList.length === 0) {
+    first.style.backgroundColor = '#3E3EC4';
+}
+
+// if (rankedList.length === 0) {
+//     first.style.backgroundColor = '#4c4cd3';
+// }
 
 // console.log(dropZone);
 
@@ -26,7 +46,7 @@ function dragEnd(){
     dragItem = null;
 }
 
-for (j of choice){
+for (var j of choice){
     j.addEventListener('dragover', dragOver);
     j.addEventListener('dragenter', dragEnter);
     j.addEventListener('dragleave', dragLeave);
@@ -78,9 +98,67 @@ function onClick() {
     if(dropZone.contains(this)){
         // console.log(this.id);
 
+        // this.className = 'card-panel'
+    
         // remove this from the list
+        
+
+        if (rankedList.length === 1) {
+            first.style.backgroundColor = '#3E3EC4'; 
+            second.style.backgroundColor = '#5757F2';
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>first</span> choice pick"
+            submitButton.style.backgroundColor = "#fff";
+        }
+        if (rankedList.length === 2) {
+            second.style.backgroundColor = '#3E3EC4';
+            third.style.backgroundColor = '#5757F2';
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>second</span> choice pick"
+            submitButton.style.backgroundColor = "#fff";
+        }
+        if (rankedList.length === 3) {
+            third.style.backgroundColor = '#3E3EC4';
+            fourth.style.backgroundColor = '#5757F2';
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>third</span> choice pick"
+            submitButton.style.backgroundColor = "#fff";
+        }
+        if (rankedList.length === 4) {
+            fourth.style.backgroundColor = '#3E3EC4';
+            fifth.style.backgroundColor = '#5757F2';
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>fourth</span> choice pick"
+            submitButton.style.backgroundColor = "#fff";
+        }
+        if (rankedList.length === 5) {
+            fifth.style.backgroundColor = '#3E3EC4';
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>fifth</span> choice pick"
+            submitButton.style.backgroundColor = "#fff";
+            submitButton.style.color = "#000";
+            // make submit button change colors
+        }
+
         const index = rankedList.indexOf(this)
         if (index > -1) { rankedList.splice(index, 1) }
+
+        // if (rankedList.length === 1) {
+        //     first.style.backgroundColor = '#5757F2';
+        //     second.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 2) {
+        //     second.style.backgroundColor = '#5757F2';
+        //     third.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 3) {
+        //     third.style.backgroundColor = '#5757F2';
+        //     fourth.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 4) {
+        //     fourth.style.backgroundColor = '#5757F2';
+        //     fifth.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 5) {
+        //     fifth.style.backgroundColor = '#5757F2';
+        //     // make submit button change colors
+        //     submitButton.style.backgroundColor = "red";
+        // }
 
         console.log(rankedList);
 
@@ -94,6 +172,9 @@ function onClick() {
         else if(this.id === "right") {
             rightSide.append(this);
         }
+        else if(this.id === "middle") {
+            middleSide.append(this);
+        }
     }
     
 
@@ -101,8 +182,91 @@ function onClick() {
         if(rankedList.length === 5) {
             return;
         }
+
         rankedList.push(this);
-        console.log(rankedList);
-        dropZone.append(this);
+
+        if (rankedList.length === 1) {
+            first.style.backgroundColor = '#5757F2'; //lighter blue? color1 
+            second.style.backgroundColor = '#3E3EC4'; //darker blue? color2
+            submitButton.style.backgroundColor = "#fff";
+            // console.log(candidates);
+            // console.log(candidates.innerHTML);
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>second</span> choice pick"
+        }
+        if (rankedList.length === 2) {
+            second.style.backgroundColor = '#5757F2';
+            third.style.backgroundColor = '#3E3EC4';
+            submitButton.style.backgroundColor = "#fff";
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>third</span> choice pick"
+        }
+        if (rankedList.length === 3) {
+            third.style.backgroundColor = '#5757F2';
+            fourth.style.backgroundColor = '#3E3EC4';
+            submitButton.style.backgroundColor = "#fff";
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>fourth</span> choice pick"
+        }
+        if (rankedList.length === 4) {
+            fourth.style.backgroundColor = '#5757F2';
+            fifth.style.backgroundColor = '#3E3EC4';
+            submitButton.style.backgroundColor = "#fff";
+            candidates.innerHTML = "Select your <span id='candidates-change-span'>fifth</span> choice pick"
+        }
+        if (rankedList.length === 5) {
+            fifth.style.backgroundColor = '#5757F2';
+            candidates.innerHTML = "Review your vote and click submit!"
+            // make submit button change colors
+            submitButton.style.backgroundColor = "#3E3EC4";
+            submitButton.style.color = "#fff";
+        }
+
+        // if (rankedList.length === 1) {
+        //     first.style.backgroundColor = '#5757F2';
+        //     second.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 2) {
+        //     second.style.backgroundColor = '#5757F2';
+        //     third.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 3) {
+        //     third.style.backgroundColor = '#5757F2';
+        //     fourth.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 4) {
+        //     fourth.style.backgroundColor = '#5757F2';
+        //     fifth.style.backgroundColor = '#4c4cd3';
+        // }
+        // if (rankedList.length === 5) {
+        //     fifth.style.backgroundColor = '#5757F2';
+        //     // make submit button change colors
+        //     submitButton.style.backgroundColor = "red";
+        // }
+        // add an animation class to it
+        // if(rankedList.length === 0) {
+        //     this.className = 'animate card-panel';
+        // }
+        // this.className = 'animate card-panel';
+        // console.log(this.className);
+        
+        dropZone.append(this)
     }
+
+    // else if (!dropZone.contains(this)) {
+    //     if(rankedList.length === 5) {
+    //         return;
+    //     }
+    //     rankedList.push(this);
+    //     console.log(rankedList);
+    //     dropZone.append(this);
+    // }
+    
 }
+
+/* $(document).ready(function() {
+    // all custom jQuery will go here
+}); */
+
+// $(document).ready(function(){
+//     $("p").click(function(){
+//       $(this).hide();
+//     });
+//   });
